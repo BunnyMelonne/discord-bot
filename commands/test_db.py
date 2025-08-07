9e26344186c89d1d581431fa465ce7c9d3c87c4b
Melon <133422815+BunnyMelonne@users.noreply.github.com>
2025-08-07 12:29:55 +0200

    Update test_db.py

--- commands/test_db.py
+++ commands/test_db.py
@@ -9,8 +9,6 @@ class TestDB(commands.Cog):
     @commands.command(name="testdb")
     async def test_db(self, ctx):
         try:
-            # On essaye d'insérer un doc temporaire (avec un champ timestamp pour ne pas écraser)
-            from datetime import datetime
             result = users_collection.insert_one({"test": "ok", "timestamp": datetime.utcnow()})
             await ctx.send(f"✅ DB ok, id doc inséré : {result.inserted_id}")
         except Exception as e:
